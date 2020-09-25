71bc822aa41b0efdd9089d718daa192e81f33384
peter-madigan <pmadigan@berkeley.edu>
2020-09-25 12:07:10 -0700

    Allow for disabling CSA in pedestal measurements

--- pedestal.py
+++ pedestal.py
@@ -8,7 +8,7 @@ import larpix.io
 import base
 import base_warm
 
-def main(controller_config=None, periodic_trigger_cycles=1281, runtime=60, channels=[0], chip_key='1-1-23', *args, **kwargs):
+def main(controller_config=None, periodic_trigger_cycles=1281, runtime=60, channels=[0], chip_key='1-1-23', disabled_channels=None, *args, **kwargs):
     # create controller
     c = base_warm.main(controller_config_file=controller_config, logger=True)
 
@@ -16,6 +16,8 @@ def main(controller_config=None, periodic_trigger_cycles=1281, runtime=60, chann
     chip_keys = [chip_key]
     if chip_key is None:
         chip_keys = list(c.chips.keys())
+    if disabled_channels is None:
+        disabled_channels = list()
 
     # set configuration
     c.io.double_send_packets = True
@@ -34,6 +36,9 @@ def main(controller_config=None, periodic_trigger_cycles=1281, runtime=60, chann
         chip.config.enable_hit_veto = 0
         #chip.config.periodic_reset_cycles = 1 # non-default
         chip.config.periodic_reset_cycles = 4096
+
+        for channel in disabled_channels:
+            chip.config.csa_enable[channel] = 0
         
         # write configuration
         registers = list(range(155,163)) # periodic trigger mask
@@ -52,9 +57,11 @@ def main(controller_config=None, periodic_trigger_cycles=1281, runtime=60, chann
         c.write_configuration(chip_key, 'enable_rolling_periodic_reset')
         c.write_configuration(chip_key, 'periodic_reset_cycles')
         c.write_configuration(chip_key, 'periodic_reset_cycles')
+        c.write_configuration(chip_key, 'csa_enable')
+        c.write_configuration(chip_key, 'csa_enable')        
 
     for chip_key in c.chips:
-        ok, diff = c.verify_configuration(chip_key)
+        ok, diff = c.verify_configuration(chip_key, timeout=0.01)
         if not ok:
             print('config error',diff)
     c.io.double_send_packets = True    
@@ -75,7 +82,8 @@ if __name__ == '__main__':
     parser.add_argument('--periodic_trigger_cycles', default=1281, type=int)
     parser.add_argument('--runtime', default=60, type=float)
     parser.add_argument('--channels', default=range(64), type=json.loads)
-    parser.add_argument('--chip_key', default=None, type=str)    
+    parser.add_argument('--chip_key', default=None, type=str)
+    parser.add_argument('--disabled_channels', default=None, type=json.loads)        
     args = parser.parse_args()
     c = main(**vars(args))
 
