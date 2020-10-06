54f07408c64a2301e5595de26a3c41d1d9301ef8
peter-madigan <pmadigan@berkeley.edu>
2020-10-06 16:37:02 -0700

    Update base.py arguments

--- base.py
+++ base.py
@@ -1,9 +1,26 @@
 import sys
 import time
+import argparse
 
 import larpix
 import larpix.io
 import larpix.logger
+
+#_vddd_dac = 0xb620 # for ~1.2V operation
+_vddd_dac = 0xd8e4 # for ~1.8V operation
+_vdda_dac = 0xd8e4
+_uart_phase = 0
+
+_default_controller_config=None
+_default_logger=False
+_default_reset=True
+
+_default_chip_id = 2
+_default_io_channel = 1
+_default_miso_ds = 0
+_default_mosi = 0
+
+_default_clk_ctrl = 1
 
 clk_ctrl_2_clk_ratio_map = {
         0: 2,
@@ -19,11 +36,11 @@ def flush_data(controller, runtime=0.1, rate_limit=0., max_iterations=10):
     '''
     for _ in range(max_iterations):
         controller.run(runtime, 'flush_data')
-        if len(controller.reads[-1])/runtime < rate_limit:
+        if len(controller.reads[-1])/runtime <= rate_limit:
             break
         
-def main(controller_config_file=None, logger=False, reset=True):
-    print('base config')
+def main(controller_config_file=_default_controller_config, logger=_default_logger, reset=_default_reset):
+    print('START BASE')
 
     # create controller
     c = larpix.Controller()
@@ -31,17 +48,8 @@ def main(controller_config_file=None, logger=False, reset=True):
 
     # set larpix power voltages (pacman only)
     print('Setting larpix power...')
-    #c.io.set_vddd()[1] # default (VDDD~1.8V)
-    #c.io.set_vddd(vddd_dac=0xeee4)[1] #
-    c.io.set_vddd(vddd_dac=0xb620)[1] # default (VDDD~1.2V)        
-    #c.io.set_vddd(vddd_dac=0xbbd0)[1] # mid-power (VDDD~1.6V)
-    #c.io.set_vddd(vddd_dac=0x95a3)[1] # low-power (VDDD~1.3V)
-    #c.io.set_vddd(vddd_dac=0x9498)[1] # low-power (VDDD~1.285V)
-
-    #c.io.set_vddd(vddd_dac=0xdee4)[1] 
-    
-    #c.io.set_vdda()[1]
-    c.io.set_vdda(vdda_dac=0xd8e4)[1]    
+    c.io.set_vddd(_vddd_dac)[1]
+    c.io.set_vdda(_vdda_dac)[1]
     mask = c.io.enable_tile()[1]
     print('tile enabled?:',hex(mask))
     vddd,iddd = c.io.get_vddd()[1]
@@ -50,33 +58,22 @@ def main(controller_config_file=None, logger=False, reset=True):
     print('IDDD:',iddd,'mA')
     print('VDDA:',vdda,'mV')
     print('IDDA:',idda,'mA')
-    phase = 0
     for ch in range(1,5):
-        c.io.set_reg(0x1000*ch + 0x2014, phase)
-    print('set phase:',phase)
+        c.io.set_reg(0x1000*ch + 0x2014, _uart_phase)
+    print('set phase:',_uart_phase)
     
     if logger:
         print('logger')
         c.logger = larpix.logger.HDF5Logger()
         print('filename:',c.logger.filename)
-        c.logger.disable()
+        c.logger.enable()
 
     if controller_config_file is None:
-        channel = 1 # fpga channel
-        chip_id = 2
-        #c.add_chip(larpix.Key(1,channel,1))
-        c.add_chip(larpix.Key(1,channel,chip_id))
-        # create node for fpga
-        c.add_network_node(1,channel,c.network_names, 'ext', root=True)
-        # create upstream link for fpga
-        #c.add_network_link(1,channel,'miso_us',('ext',1),1)
-        c.add_network_link(1,channel,'miso_us',('ext',chip_id),0)
-        # create downstream link on miso_0
-        #c.add_network_link(1,channel,'miso_ds',(1,'ext'),1)
-        c.add_network_link(1,channel,'miso_ds',(chip_id,'ext'),0)
-        # create mosi link on mosi_1
-        #c.add_network_link(1,channel,'mosi',('ext',1),1)
-        c.add_network_link(1,channel,'mosi',('ext',chip_id),0)
+        c.add_chip(larpix.Key(1, _default_io_channel, _default_chip_id))
+        c.add_network_node(1, _default_io_channel, c.network_names, 'ext', root=True)
+        c.add_network_link(1, _default_io_channel, 'miso_us', ('ext',_default_chip_id), 0)
+        c.add_network_link(1, _default_io_channel, 'miso_ds', (_default_chip_id,'ext'), _default_miso_ds)
+        c.add_network_link(1, _default_io_channel, 'mosi', ('ext', _default_chip_id), _default_mosi)
 
     else:
         c.load(controller_config_file)
@@ -99,20 +96,18 @@ def main(controller_config_file=None, logger=False, reset=True):
             c.init_network(io_group, io_channel, modify_mosi=False)
     
     # set uart speed
-    clk_ctrl = 1
     print('set uart speed and diff signaling...')
     for io_group, io_channels in c.network.items():
         for io_channel in io_channels:
             chip_keys = c.get_network_keys(io_group,io_channel,root_first_traversal=False)
             for chip_key in chip_keys:
-                print('set',chip_key)
-                c[chip_key].config.clk_ctrl = clk_ctrl
+                c[chip_key].config.clk_ctrl = _default_clk_ctrl
                 c.write_configuration(chip_key, 'clk_ctrl')
     for io_group, io_channels in c.network.items():
         for io_channel in io_channels:
-            print(io_channel, c.io.set_uart_clock_ratio(io_channel, clk_ctrl_2_clk_ratio_map[clk_ctrl], io_group=io_group))
+            print('io_channel:',io_channel,'factor:',c.io.set_uart_clock_ratio(io_channel, clk_ctrl_2_clk_ratio_map[_default_clk_ctrl], io_group=io_group))
 
-    # set other configuration registers
+    # set any other configuration registers
     for chip_key in c.chips:
         registers = []
         register_map = c[chip_key].config.register_map
@@ -129,15 +124,22 @@ def main(controller_config_file=None, logger=False, reset=True):
 
     # verify
     c.io.double_send_packets = True
-    ok,diff = c.verify_network(timeout=0.01)
-    for chip_key in diff:
-        ok,diff = c.verify_network(chip_key,timeout=0.01)
+    for chip_key in c.chips:
+        ok,diff = c.verify_registers([(chip_key,0)],timeout=0.01)
         if not ok:
             for key in diff:
                 print('config error',key,diff[key])
     c.io.double_send_packets = False
     c.io.group_packets_by_io_group = True
+
+    print('END BASE')
     return c
 
 if __name__ == '__main__':
-    c = main(*sys.argv[1:])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--controller_config', default=_default_controller_config, type=str, help='''Hydra network configuration file''')
+    parser.add_argument('--logger', default=_default_logger, action='store_true', help='''Flag to create an HDF5Logger object to track data''')
+    parser.add_argument('--no_reset', default=_default_reset, action='store_false', help='''Flag that if present, chips will NOT be reset, otherwise chips will be reset during initialization''')    
+    args = parser.parse_args()
+    c = main(**vars(args))
+
