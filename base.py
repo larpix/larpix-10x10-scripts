93a1ad20c00f432d81253bb0b8f40b03db0c41b4
lhepdaq2 <none>
2020-11-25 09:14:12 +0100

    Bug fix of chip configuration (by Peter).

--- base.py
+++ base.py
@@ -6,7 +6,7 @@ import larpix
 import larpix.io
 import larpix.logger
 
-#_vddd_dac = 0xb620 # for ~1.2V operation
+#_vddd_dac = 0xb620 # for ~1.5V operation
 _vddd_dac = 0xd8e4 # for ~1.8V operation
 _vdda_dac = 0xd8e4
 _uart_phase = 0
@@ -38,7 +38,7 @@ def flush_data(controller, runtime=0.1, rate_limit=0., max_iterations=10):
         controller.run(runtime, 'flush_data')
         if len(controller.reads[-1])/runtime <= rate_limit:
             break
-        
+
 def main(controller_config=_default_controller_config, logger=_default_logger, reset=_default_reset, **kwargs):
     print('START BASE')
 
@@ -53,7 +53,7 @@ def main(controller_config=_default_controller_config, logger=_default_logger, r
     c.io.set_vddd(_vddd_dac)[1]
     c.io.set_vdda(_vdda_dac)[1]
     vddd,iddd = c.io.get_vddd()[1]
-    vdda,idda = c.io.get_vdda()[1] 
+    vdda,idda = c.io.get_vdda()[1]
     print('VDDD:',vddd,'mV')
     print('IDDD:',iddd,'mA')
     print('VDDA:',vdda,'mV')
@@ -61,7 +61,7 @@ def main(controller_config=_default_controller_config, logger=_default_logger, r
     for ch in range(1,5):
         c.io.set_reg(0x1000*ch + 0x2014, _uart_phase)
     print('set phase:',_uart_phase)
-    
+
     if logger:
         print('logger')
         c.logger = larpix.logger.HDF5Logger()
@@ -89,12 +89,12 @@ def main(controller_config=_default_controller_config, logger=_default_logger, r
                 c.io.set_uart_clock_ratio(io_channel, clk_ctrl_2_clk_ratio_map[0], io_group=io_group)
 
     # initialize network
-    c.io.group_packets_by_io_group = False # throttle the data rate to insure no FIFO collisions  
+    c.io.group_packets_by_io_group = False # throttle the data rate to insure no FIFO collisions
     for io_group, io_channels in c.network.items():
         for io_channel in io_channels:
             print('init network {}-{}'.format(io_group,io_channel))
             c.init_network(io_group, io_channel, modify_mosi=False)
-    
+
     # set uart speed
     print('set uart speed and diff signaling...')
     for io_group, io_channels in c.network.items():
@@ -117,7 +117,7 @@ def main(controller_config=_default_controller_config, logger=_default_logger, r
         registers += list(register_map['vcm_dac'])
         c[chip_key].config.vcm_dac = 41
         c[chip_key].config.csa_gain = 0 # high gain
-        #c[chip_key].config.csa_gain = 1 # low gain        
+        #c[chip_key].config.csa_gain = 1 # low gain
         registers += list(register_map['csa_gain'])
         c[chip_key].config.adc_hold_delay = 15
         registers += list(register_map['adc_hold_delay'])
@@ -146,7 +146,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--controller_config', default=_default_controller_config, type=str, help='''Hydra network configuration file''')
     parser.add_argument('--logger', default=_default_logger, action='store_true', help='''Flag to create an HDF5Logger object to track data''')
-    parser.add_argument('--no_reset', default=_default_reset, action='store_false', help='''Flag that if present, chips will NOT be reset, otherwise chips will be reset during initialization''')    
+    parser.add_argument('--no_reset', default=_default_reset, action='store_false', help='''Flag that if present, chips will NOT be reset, otherwise chips will be reset during initialization''')
     args = parser.parse_args()
     c = main(**vars(args))
 
